28e79a52ac30cb8c4895b210d71e75c70e11709a
devoid-of-thought <mikynowy@gmail.com>
2026-05-07 17:41:39 +0200

    fix: added docstring

--- utils.py
+++ utils.py
@@ -1,11 +1,17 @@
-def add(a: int, b: int) -> int:
+"""Functions of simple calculator operations"""
+
+def add (a: int , b: int) -> int :
+    """Adding Two numbers"""
     return a + b
 
-def subtract(a: int, b: int) -> int:
+def subtract (a: int , b: int) -> int :
+    """Subtracting Two numbers"""
     return a - b
 
-def multiply(a: int, b: int) -> int:
+def multiply (a: int , b: int) -> int :
+    """Multiplying Two numbers"""
     return a * b
 
-def divide(a: int, b: int) -> float:
+def divide ( a: int , b : int ) -> float :
+    """Dividing Two numbers"""
     return a / b
